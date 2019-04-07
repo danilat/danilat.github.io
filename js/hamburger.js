898d54a6827ebf06c8c5592cd6781db581fc9e3c
Dani Latorre <dani@danilat.com>
2019-04-08 00:59:11 +0200

    the menu and cross link for mobile are by default hidden

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,4 @@
 $( document ).ready(function() {
-  $( ".cross" ).hide();
-  $( ".menu" ).hide();
   $( ".hamburger" ).click(function() {
     $( ".menu" ).slideToggle( "slow", function() {
     $( ".hamburger" ).hide();
